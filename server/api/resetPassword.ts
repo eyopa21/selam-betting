c1dc300664c306986f0f6b7faac80444a38bfd6f
zack <zekariyasmulugeta91@gmail.com>
2024-07-02 17:05:16 +0300

    cleanups

--- server/api/resetPassword.ts
+++ server/api/resetPassword.ts
@@ -2,9 +2,6 @@ export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig();
     const body = await readBody(event)
     const url = `${config.restApiEndpoint}/reset_password/`;
-    console.log(JSON.stringify(body));
-    console.log(url);
-    console.log(config.serverApiKey);
 
     try {
         const response = await fetch(url, {
@@ -15,7 +12,6 @@ export default defineEventHandler(async (event) => {
             },
             body: JSON.stringify(body)
         })
-        console.log(response);
 
         const data = await response.json()
 
