41082199e923d3dc9de958b2e253298f2db9d164
joba <jobtennis21@gmail.com>
2024-07-03 00:21:15 +0300

    chore: basic changes

--- pages/index.vue
+++ pages/index.vue
@@ -20,13 +20,15 @@ const { data: recommendedGames } = await useFetch(
 );
 
 const setList = async () => {
-  const filteredMatches = await recommendedGames.value?.data.results.map(
+  const filteredMatches = await recommendedGames.value?.data?.results.map(
     (game: any) => {
+      // console.log("recc", recommendedGames.value);
       return {
         id: game.id,
         league: game.parent_name,
         teams: game.name,
         date: convertToDateString(game.startTime),
+        time: new Date(game.startTime).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' }),
         odds: extractOdds(game.markets, game.name),
       };
     }
