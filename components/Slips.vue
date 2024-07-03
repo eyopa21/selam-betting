10f592d3b36c48831ad22ae82952607167b82691
joba <jobtennis21@gmail.com>
2024-07-03 11:41:59 +0300

    fix: displaying ticket after success done

--- components/Slips.vue
+++ components/Slips.vue
@@ -31,12 +31,11 @@
                 <p class="tw-text-xs tw-text-gray-300">{{ game.value }}</p>
                 <p>{{ game.odd }}</p>
               </div>
-              <div v-if="slipType === 'Multiple/Single'"
-                class="tw-mt-4 tw-flex tw-items-center tw-justify-around tw-h-6">
+              <div v-if="slipType === 'Single'" class="tw-mt-4 tw-flex tw-items-center tw-justify-around tw-h-6">
                 <div class="tw-bg-gray-400 tw-w-12 tw-h-6 tw-flex tw-justify-around tw-items-center tw-rounded-l">
                   <q-icon name="-" />
                 </div>
-                <q-input v-model="findSlipById.birr" dense input-class="tw-text-white tw-max-h-6 text-center"
+                <q-input v-model="game.birr" dense input-class="tw-text-white tw-max-h-6 text-center"
                   class="dark:tw-bg-primary-700 tw-bg-white tw-text-white tw-w-full tw-max-h-full" />
                 <div class="tw-bg-gray-400 tw-w-12 tw-h-6 tw-flex tw-justify-around tw-items-center tw-rounded-r">
                   <q-icon name="add" />
@@ -46,9 +45,8 @@
             </div>
             <q-separator color="gray" class=" tw-mt-16 " />
             <div class="tw-py-2">
-
               <q-select bg-color="white" label-color="white" outlined v-model="slipType"
-                :options="['Accumulator', 'Multiple/Single']" dense options-dense />
+                :options="['Accumulator', 'Single']" dense options-dense />
             </div>
 
             <div v-if="slipType === 'Accumulator'" class="tw-mt-4 tw-flex tw-items-center tw-justify-around tw-h-6">
@@ -101,14 +99,47 @@
           <div class="tw-flex tw-gap-2">
             <q-btn :disabled="!findSlipById.games?.length || findSlipById.birr < 10" @click="showTerms = true"
               label="BOOK" unelevated class="tw-font-bold tw-bg-primary-600 tw-w-full" />
-            <q-btn :disabled="!findSlipById.games?.length || findSlipById.birr < 10" color="orange" label="PLACE"
-              unelevated class="tw-font-bold tw-bg-primary-600 tw-w-full" />
+            <q-btn @click="showTerms = true" color="orange" label="PLACE" unelevated
+              class="tw-font-bold tw-bg-primary-600 tw-w-full" />
           </div>
         </div>
       </div>
     </div>
     <TermsAndPolicy :loading="loading" :showDialog="showTerms" @confirm="bookBet()" @close="showTerms = false" />
+    <q-dialog v-model="showPreviewModal" full-height>
+      <q-card v-if="preview" class="column full-height" style="width: 300px">
+        <q-card-section>
+          <div class="text-h6">Success</div>
+        </q-card-section>
+        <q-card-section>
+          <div class="text-h6">
+            <p>
+              Booking Code: {{ preview.booking_code }}
+              <br>
+              Stake: {{ parseInt(preview.stake_amount).toFixed(2) }} ETB
+              <br>
+              Possible Win: {{ preview.possible_win?.toFixed(2) }} ETB
+            </p>
+          </div>
+        </q-card-section>
 
+        <q-card-section v-for="(event, key) in preview.selected_events" :key="key" class="col q-pt-none ">
+          <div class="tw-flex tw-text-xs tw-items-center tw-justify-between ">
+            <p>{{ event.event.name }}, {{ event.event.parent_name }}</p>
+            <p class="tw-text-[10px]">{{ formatDate(event.event.startTime.split('T')[0]) }} </p>
+
+          </div>
+          <div class="tw-flex tw-items-center tw-justify-between">
+            <p class="tw-text-xs tw-text-gray-300">{{ event.outcome.name }}</p>
+            <p>{{ parseInt(event.odds).toFixed(2) }} </p>
+          </div>
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white text-teal">
+          <q-btn @click="preview = null" flat label="OK" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
     <div>
 
 
@@ -121,6 +152,13 @@
 import { useQuasar } from 'quasar'
 const $q = useQuasar()
 
+
+const preview = ref(null)
+const showPreviewModal = computed(() => {
+  return !!preview.value
+});
+
+
 const slipType = ref('Accumulator')
 const { formatDate } = useHelpers()
 const slips = ref(useSlips().slips);
@@ -130,51 +168,91 @@ const { activeSlip, addSlip, findSlipById, slipCount, calculateTotalOdd, removeS
 const showTerms = ref(false)
 
 
-
+watch(slipType, () => {
+  console.log("hello type")
+  if (slipType.value === 'Single') {
+    findSlipById.value.games = findSlipById.value.games?.map(game => {
+      return { ...game, birr: 0 }
+    })
+  } else {
+    findSlipById.value.games = findSlipById.value.games?.map(({ birr, ...rest }) => rest);
+  }
+})
 
 
 
 const loading = ref(false)
 async function bookBet() {
   loading.value = true;
-  console.log("good booking");
-  const { data, error } = await useFetch('/api/ticket', {
-    method: 'POST',
-    body: {
-      ticketType: 'Booked',
-      slipType: 'Accumulator',
-      games: findSlipById.value.games?.map(game => {
-        return {
-          event_id: BigInt(game.eventId).toString(),
-          outcome_id: BigInt(game.outcomeId).toString()
-        }
-      }),
+  if (slipType.value === 'Accumulator') {
+    const { data, error } = await useFetch('/api/ticket', {
+      method: 'POST',
+      body: {
+        ticketType: 'Booked',
+        slipType: 'Accumulator',
+        games: findSlipById.value.games?.map(game => {
+          return {
+            event_id: BigInt(game.eventId).toString(),
+            outcome_id: BigInt(game.outcomeId).toString()
+          }
+        }),
 
-      amount: findSlipById.value.birr
+        amount: findSlipById.value.birr
+      }
+    })
+    if (error.value) {
+      loading.value = false
+      $q.notify({
+        message: 'Booking Error',
+        color: 'red',
+        position: 'right'
+      })
     }
-  })
-  console.log("data", data);
-  console.log("error", error);
-  if (error.value?.data) {
-    loading.value = false
-    $q.notify({
-      message: 'Booking Error',
-      color: 'red',
-      position: 'right'
-    })
-  }
-  else if (data.value) {
+    else if (data.value) {
+      preview.value = data.value.data
+      loading.value = false
 
-    loading.value = false
-    $q.notify({
-      message: 'Booking success',
-      caption: `Your Booking Code is: ${data.value?.data?.booking_code}`,
-      color: 'green',
-      position: 'right'
+      showTerms.value = false
+      findSlipById.value.games = []
+    }
+  } else if (slipType.value === 'Single') {
+
+    console.log("single betting")
+    const { data, error } = await useFetch('/api/ticket', {
+      method: 'POST',
+      body: {
+        ticketType: 'Booked',
+        slipType: 'Single',
+        games: findSlipById.value.games?.map(game => {
+          return {
+            event_id: BigInt(game.eventId).toString(),
+            outcome_id: BigInt(game.outcomeId).toString(),
+            stake: game.birr
+          }
+        }),
+      }
     })
-    showTerms.value = false
-    findSlipById.value.games = []
+    console.log("error", error.value);
+    console.log("data", data.value);
+    if (error.value) {
+      loading.value = false
+      $q.notify({
+        message: 'Booking Error',
+        color: 'red',
+        position: 'right'
+      })
+    }
+    else if (data.value) {
+      preview.value = data.value.data
+      loading.value = false
+      showTerms.value = false
+      findSlipById.value.games = []
+    }
   }
+}
+
+async function placeBet() {
+  console.log("place", findSlipById.value);
 }
 
 
