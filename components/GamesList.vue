05406ab80fda0f2248d4a23cd65d1334a3f293a2
joba <jobtennis21@gmail.com>
2024-07-25 11:23:40 +0300

    feat, chore: basic of matches infinite scroll
    removing the tab panel that was causing the infinte scroll error
    
    and removing unused declarations

--- components/GamesList.vue
+++ components/GamesList.vue
@@ -72,21 +72,12 @@ const sport_categories = ref([
 
 
 const matchListStore = useMatchListStore();
-const matches = ref(matchListStore.listOfMatches)
-const openedMarkets = ref<string[]>([]);
-const aciveHeader: Ref<string> = ref("Matches");
+
+const activeHeader: Ref<string> = ref("Matches");
 
 const liveStreamToggle: Ref<boolean> = ref(false);
 
 const headersArray = ["Matches", "Recommended", "Upcoming Event"];
-import type { Matches, Participants } from '~/types/matches';
-
-
-const activate_sport = (sport) => {
-  const index = sport_categories.value.findIndex(t => t.label === sport.label)
-  sport_categories.value.forEach((t) => t.selected = false)
-  sport_categories.value[index].selected = true
-}
 
 
 
@@ -100,6 +91,16 @@ const getMatches = computed(() => {
   }
 })
 
+const page = ref(2);
+const loadMore = async (index: number, done: () => void) => {
+  console.log("index", index);
+  setTimeout(async () => {
+
+    page.value++;
+    done();
+  }, 5000)
+};
+
 </script>
 <template>
   <div>
@@ -110,7 +111,7 @@ const getMatches = computed(() => {
 
       </div>
       <div class="tw-items-start tw-ml-4">
-        <q-tabs v-model="aciveHeader" inline-label outside-arrows mobile-arrows dense>
+        <q-tabs v-model="activeHeader" inline-label outside-arrows mobile-arrows dense>
           <q-tab class="tw-text-white tw-font-semibold tw-text-xl" v-for="header in headersArray" :name="header"
             :label="header" />
         </q-tabs>
@@ -139,90 +140,91 @@ const getMatches = computed(() => {
     <div v-if="!getMatches?.length">
       <VUEEmptyState />
     </div>
-    <q-tab-panels v-else v-model="aciveHeader" animated>
-      <q-tab-panel name="Matches" class="tw-p-2 tw-bg-secondary-800 dark:tw-bg-gray-900">
-        <q-infinite-scroll v-if="getMatches?.length" :offset="500">
+    <div v-else>
 
 
-          <div class="tw-rounded">
-            <div v-for="(match, key) in getMatches " :key="key"
-              class="tw-mb-3 tw-border-2 tw-rounded-md tw-p-2 tw-bg-gray-800 tw-border-gray-900">
-              <div class="tw-flex tw-justify-start dark:text-white tw-items-end">
-                <div class="tw-w-[40%] tw-mr-3 tw-border-r-2 tw-border-gray-600 tw-pr-3 tw-px-3 tw-py-3">
-                  <div class="tw-flex tw-justify-between">
-                    <p class="tw-font-semibold tw-text-gray-700 dark:tw-text-gray-400">
-                      {{ match.league.split(',')[1] }} | {{ match.league.split(',')[0] }}
-                    </p>
-                    <p class="tw-flex tw-flex-col tw-text-xs tw-text-end tw-text-gray-700  dark:tw-text-gray-300">
-                      <span>{{ formatDate(match.date) }} <span class="tw-bg-green-700 tw-p-1 tw-rounded-md">{{
-                        match.time }}</span> </span>
-                      <span></span>
-                    </p>
-                  </div>
-                  <div v-if="match.participants?.length"
-                    class="tw-flex tw-mt-4 tw-gap-2 tw-text-sm tw-text-gray-700 dark:tw-text-gray-200">
-                    <span class="tw-flex tw-gap-2">
-                      <img class="tw-size-8" :src="match.participants[0].club.logo"
-                        :alt="match.participants[0]?.club.name" />
-                      {{ match.participants[0]?.club.name }}
-                    </span>
-                    <span>Vs</span>
-                    <span class="tw-flex tw-gap-2">
-                      <img class="tw-size-8" :src="match.participants[1].club.logo"
-                        :alt="match.participants[1]?.club.name" />
-                      {{ match.participants[1]?.club.name }}
-                    </span>
-                  </div>
+      <q-tab-panels v-model="activeHeader" animated class="tw-h-full">
+        <q-tab-panel name="Recommended">recommended matches </q-tab-panel>
+        <q-tab-panel name="Upcoming Event" class="tw-bg-gray-900">
+          <UpcomingEvents :matches="getMatches" />
+        </q-tab-panel>
+      </q-tab-panels>
+      <q-infinite-scroll v-if="getMatches?.length && activeHeader === 'Matches'" @load="loadMore" :offset="100" class=" ">
 
+
+        <div class="tw-rounded">
+          <div v-for="(match, key) in getMatches " :key="key"
+            class="tw-mb-3 tw-border-2 tw-rounded-md tw-p-2 tw-bg-gray-800 tw-border-gray-900">
+            <div class="tw-flex tw-justify-start dark:text-white tw-items-end">
+              <div class="tw-w-[40%] tw-mr-3 tw-border-r-2 tw-border-gray-600 tw-pr-3 tw-px-3 tw-py-3">
+                <div class="tw-flex tw-justify-between">
+                  <p class="tw-font-semibold tw-text-gray-700 dark:tw-text-gray-400">
+                    {{ match.league.split(',')[1] }} | {{ match.league.split(',')[0] }}
+                  </p>
+                  <p class="tw-flex tw-flex-col tw-text-xs tw-text-end tw-text-gray-700  dark:tw-text-gray-300">
+                    <span>{{ formatDate(match.date) }} <span class="tw-bg-green-700 tw-p-1 tw-rounded-md">{{
+                      match.time }}</span> </span>
+                    <span></span>
+                  </p>
+                </div>
+                <div v-if="match.participants?.length"
+                  class="tw-flex tw-mt-4 tw-gap-2 tw-text-sm tw-text-gray-700 dark:tw-text-gray-200">
+                  <span class="tw-flex tw-gap-2">
+                    <img class="tw-size-8" :src="match.participants[0].club.logo"
+                      :alt="match.participants[0]?.club.name" />
+                    {{ match.participants[0]?.club.name }}
+                  </span>
+                  <span>Vs</span>
+                  <span class="tw-flex tw-gap-2">
+                    <img class="tw-size-8" :src="match.participants[1].club.logo"
+                      :alt="match.participants[1]?.club.name" />
+                    {{ match.participants[1]?.club.name }}
+                  </span>
                 </div>
 
-
-                <div class="tw-grid tw-grid-cols-7 tw-gap-2 tw-mb-2 tw-w-[60%]">
-                  <div v-for="odd in match.odds" class="tw-w-full tw-mr-2">
-                    <Odd :matchDetail="{
-                      id: match.id,
-                      teams: match.teams,
-                      league: match.league,
-                      date: match.date,
-                      time: match.time,
-                    }" :outcomeId="odd.outcomeId" :eventId="odd.eventId" :value="odd.value" :odd="odd.odd" />
-                  </div>
-                  <q-btn outline dense padding="xs lg" size="sm" color="green"
-                    class="tw-rounded-lg  dark:tw-text-white tw-whitespace-nowrap tw-text-gray-600 tw-font-semibold"
-                    :label="`+${match.numberOfMarkets}`" @click="  $router.push(`/matches/${match.id}`)">
-                    <q-tooltip class="bg-green">View all <span class="tw-underline">{{ match.numberOfMarkets }}</span>
-                      Markets</q-tooltip>
-
-                  </q-btn>
-                </div>
               </div>
 
 
+              <div class="tw-grid tw-grid-cols-7 tw-gap-2 tw-mb-2 tw-w-[60%]">
+                <div v-for="odd in match.odds" class="tw-w-full tw-mr-2">
+                  <Odd :matchDetail="{
+                    id: match.id,
+                    teams: match.teams,
+                    league: match.league,
+                    date: match.date,
+                    time: match.time,
+                  }" :outcomeId="odd.outcomeId" :eventId="odd.eventId" :value="odd.value" :odd="odd.odd" />
+                </div>
+                <q-btn outline dense padding="xs lg" size="sm" color="green"
+                  class="tw-rounded-lg  dark:tw-text-white tw-whitespace-nowrap tw-text-gray-600 tw-font-semibold"
+                  :label="`+${match.numberOfMarkets}`" @click="  $router.push(`/matches/${match.id}`)">
+                  <q-tooltip class="bg-green">View all <span class="tw-underline">{{ match.numberOfMarkets }}</span>
+                    Markets</q-tooltip>
 
-              <div v-if="match.showMarket">
-                <AllMarkets :matchDetail="{
-                  id: match.id,
-                  teams: match.teams,
-                  league: match.league,
-                  date: match.date,
-                  time: match.time,
-                }" />
-
+                </q-btn>
               </div>
+            </div>
+
+
+
+            <div v-if="match.showMarket">
+              <AllMarkets :matchDetail="{
+                id: match.id,
+                teams: match.teams,
+                league: match.league,
+                date: match.date,
+                time: match.time,
+              }" />
+
             </div>
           </div>
-          <template v-slot:loading>
-            <div class="row justify-center q-my-md">
-              <q-spinner-dots color="primary" size="40px" />
-            </div>
-          </template>
-        </q-infinite-scroll>
-
-      </q-tab-panel>
-      <q-tab-panel name="Recommended">recommended matches </q-tab-panel>
-      <q-tab-panel name="Upcoming Event" class="tw-bg-gray-900">
-        <upcomming-events :matches="getMatches" />
-      </q-tab-panel>
-    </q-tab-panels>
+        </div>
+        <template v-slot:loading>
+          <div class="row justify-center q-my-md">
+            <q-spinner-dots color="primary" size="40px" />
+          </div>
+        </template>
+      </q-infinite-scroll>
+    </div>
   </div>
 </template>
