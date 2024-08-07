a3df579e978acab759606f75ed2afb9083f76987
joba <jobtennis21@gmail.com>
2024-08-07 09:17:10 +0300

    wip: bet history page

--- pages/account/bet-history.vue
+++ pages/account/bet-history.vue
@@ -7,7 +7,9 @@ definePageMeta({
 
 <template>
     <div>
-        <h4 class="tw-text-lg">Bet History</h4>
-        <p>More information about your bets</p>
+        <div class="tw-font-bold">
+            <h4 class="tw-text-lg">Bet History</h4>
+            <p>More information about your bets</p>
+        </div>
     </div>
 </template>
