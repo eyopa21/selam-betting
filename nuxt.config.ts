93462f2388caadd9a4dee7073c5bec1174b129a8
joba <jobtennis21@gmail.com>
2024-08-25 14:36:28 +0300

    feat: adding eslint package and module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-quasar-ui", "@nuxtjs/tailwindcss", "@nuxt/image", '@pinia/nuxt', '@vueuse/nuxt',],
+  modules: [
+    "nuxt-quasar-ui",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    "@nuxt/eslint"
+  ],
   css: ["~/assets/css/main.css"],
 
   quasar: {
