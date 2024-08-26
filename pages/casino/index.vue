0cbb56b4f101e25a41201d145aeafe5c36ef9a54
joba <jobtennis21@gmail.com>
2024-08-26 12:13:04 +0300

    wip: casino page

--- pages/casino/index.vue
+++ pages/casino/index.vue
@@ -107,12 +107,13 @@ const selectedGameLink  = ref('')
                     class="text-white shadow-1  rounded-border">
                     <q-carousel-slide name="1" img-src="/casinoImage.png"
                         class="tw-w-full tw-h-full"></q-carousel-slide>
-                    
+
                 </q-carousel>
             </div>
             <q-scroll-area class="tw-w-1/3 tw-overflow-auto tw-max-h-[300px] tw-gap-2">
                 <div v-for="i in 7" class="tw-flex tw-flex-col tw-space-y-8 tw-gap-y-4 ">
-                    <div  class="tw-flex tw-justify-between tw-shadow-sm tw-shadow-gray-400 tw-ml-1 tw-gap-4 tw-bg-primary-900 tw-h-20 tw-rounded-lg tw-p-2 tw-my-1 ">
+                    <div
+                        class="tw-flex tw-justify-between tw-shadow-sm tw-shadow-gray-400 tw-ml-1 tw-gap-4 tw-bg-primary-900 tw-h-20 tw-rounded-lg tw-p-2 tw-my-1 ">
                         <q-img :src="games[0].image" alt="img" class="tw-size-16 tw-rounded" />
                         <div class="tw-flex tw-flex-col text-white tw-text-xs tw-w-1/2">
                             <p class="tw-text-primary-300 tw-text-base">Congratulations</p>
@@ -130,7 +131,7 @@ const selectedGameLink  = ref('')
         </div>
         <div class="tw-flex tw-my-8 tw-justify-center">
             <q-tabs narrow-indicator dense align="justify" class="text-white">
-                <q-route-tab name="mails" icon="home" label="LOBBY"  />
+                <q-route-tab name="mails" icon="home" label="LOBBY" />
                 <q-route-tab name="virtual" icon="add_to_queue" label="VIRTUAL" />
                 <q-route-tab name="league" icon="api" label=" LEAGUE" />
                 <q-route-tab name="crash" icon="videogame_asset" label="CRASH GAMES" />
@@ -144,17 +145,27 @@ const selectedGameLink  = ref('')
 
             </q-tabs>
         </div>
-        <div class="tw-flex  tw-mt-6 tw-justify-center tw-gap-4">
+        <div class="tw-flex tw-mt-6 tw-justify-between tw-gap-4 tw-px-8">
+            <div class="tw-self-end tw-text-lg tw-text-white">
+                HarifSport - Special
+            </div>
+            <div class="tw-flex tw-gap-2">
+                <q-input  dense standout="text-blue-grey-5" input-class="text-white"
+                    placeholder="Search for your Games" >
+                    <template v-slot:prepend>
+                        <q-icon name="search" color="blue-grey-2" />
+                    </template>
+                </q-input>
+                <q-btn dense class="tw-h-8" color="primary" label="Let's Look" text-color="blue-grey-2" />
+            </div>
 
-            <q-input outlined standout="text-blue-grey-5" input-class="text-white" placeholder="Search for your Games"
-                class="tw-bg-primary-700 tw-w-72 ">
-                <template v-slot:prepend>
-                    <q-icon name="search" color="blue-grey-2" />
-                </template>
-            </q-input>
-            <q-btn color="primary" label="Let's Look" text-color="blue-grey-2" />
-
-
+            <div class="tw-flex1 tw-self-center tw-border-[2px] tw-rounded-lg tw-border-blue-500">
+                <q-btn-group outline stretch>
+                    <q-btn color="primary-10" icon="timeline" size="lg" />
+                    <q-btn color="primary-10" icon="visibility" size="lg" />
+                    <q-btn color="primary-10" icon="update" size="lg" />
+                </q-btn-group>
+            </div>
         </div>
         <div class="tw-p-8">
             <div class="tw-grid tw-grid-cols-4 tw-gap-4">
