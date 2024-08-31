185cc7e36b475ecc548ac8a2d6a4fbe4960d752f
joba <jobtennis21@gmail.com>
2024-08-31 18:20:39 +0300

    chore: working version of eslint configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,9 @@ export default defineNuxtConfig({
 
   },
 
+  eslint: {
+    config: { stylistic: false, standalone: false },
+  },
   runtimeConfig: {
     public: {
       financeApiEndpoint: ''
