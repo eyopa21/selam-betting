32cd4856ba6984dc4571c95a1f4d7fb3b8065541
job <jobtennis21@gmail.com>
2024-09-06 10:00:24 +0300

    feat: correct way of error handling

---
+++ server/api/auth/user-info.ts
@@ -0,0 +1,32 @@
+import type { NuxtError } from 'nuxt/app'
+import { FetchError } from 'ofetch'
+import type { Root } from '~/types/auth/user-info'
+
+type ErrorResponse = {
+  detail: string
+}
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig()
+  const url = `${config.restApiEndpoint}/user_account/`
+  const authHeader = getHeader(event, 'authorization')
+
+  try {
+    const result = await $fetch<Root>(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': config.serverApiKey,
+        'Authorization': authHeader!.toString()!,
+      },
+    })
+    return result
+  } catch (err: unknown) {
+    const error = err as NuxtError
+    const errorResponse = error.data as ErrorResponse
+    throw createError({
+      statusCode: error.statusCode,
+      statusMessage: errorResponse.detail,
+
+    })
+  }
+})
