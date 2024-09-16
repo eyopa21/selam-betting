407ef46111e7d773562f9d8a958149a76b1c38fe
job <jobtennis21@gmail.com>
2024-09-16 20:56:30 +0300

    fix: using Notify outof setup context

--- composables/useErrorNotifications.ts
+++ composables/useErrorNotifications.ts
@@ -1,16 +1,16 @@
-import { useQuasar } from 'quasar'
+import { Notify } from 'quasar'
 import type { NuxtError } from '#app'
 
 export function useErrorNotifications(
   error: Ref<unknown>,
 ) {
-  const $q = useQuasar()
   const err = error.value as NuxtError
   // if (error.value && error.value.statusCode === 401) {
   //   error.value.message = error.value.message
   // }
 
-  $q.notify({
+  Notify.create({
     message: err?.message,
+    color: 'red',
   })
 }
