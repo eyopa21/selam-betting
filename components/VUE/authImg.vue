98b1c529e5fd7096560cacf60dc9bd0a6db57234
job <jobtennis21@gmail.com>
2024-09-19 10:49:37 +0300

    refactor: decoupling
     by making this component to handle only image rendering

--- components/VUE/authImg.vue
+++ components/VUE/authImg.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-import { useDocumentVisibility } from '@vueuse/core'
-
 const props = defineProps<{
   url: string
-  name: string
+  name?: string
 }>()
-const emit = defineEmits<{
-  pay: [string, number]
-}>()
-const documentVisibility = useDocumentVisibility()
-const isTabActive = computed(() =>
-  documentVisibility.value === 'visible',
-)
+
 const getName = computed(() => {
   if (props.name === 'COMMERCIAL BANK OF ETHIOPIA') {
     return 'CBE'
@@ -21,13 +13,7 @@ const getName = computed(() => {
 })
 const { $authentication } = useNuxtApp()
 const imageData = ref<string | null>(null)
-const state = ref({
-  amount: 0,
 
-})
-const loading = ref(false)
-const isOpen = ref(false)
-const form = ref()
 onMounted(async () => {
   try {
     const response = await $fetch<Blob>(`http://162.55.223.95:8000${props.url}`, {
@@ -45,80 +31,16 @@ onMounted(async () => {
     console.error('Failed to fetch the image:', err)
   }
 })
-
-function onSubmit() {
-  loading.value = true
-  emit('pay', props.name, state.value.amount)
-}
-
-watch(isTabActive, () => {
-  loading.value = false
-  isOpen.value = false
-  form.value.reset()
-})
 </script>
 
 <template>
-  <div class="tw-cursor-pointer tw-transition-all tw-duration-500 hover:tw-scale-105" @click="isOpen = true">
+  <div v-if="props.name" class="tw-cursor-pointer tw-transition-all tw-duration-500 hover:tw-scale-105">
     <q-img v-if="imageData" fit="scale-down" :src="imageData" :alt="props.name" class="tw-h-20 tw-w-32" />
     <div class="tw-w-full tw-bg-primary-500 tw-text-center tw-capitalize tw-text-white">
       {{ getName }}
     </div>
   </div>
-  <q-dialog v-model="isOpen">
-    <div>
-      <q-form
-        ref="form"
-        class="q-gutter-md"
-        @submit="onSubmit"
-      >
-        <q-card class="tw-p-4">
-          <q-card-section>
-            <div class="text-h6">
-              <q-img v-if="imageData" :src="imageData" fit="contain" class="tw-h-20 tw-w-full" />
-            </div>
-          </q-card-section>
-
-          <q-separator />
-
-          <q-card-section style="max-height: 50vh" class="scroll tw-my-8">
-            <div class="tw-grid tw-grid-cols-2 tw-gap-8">
-              <label for="available" class="tw-flex   tw-flex-col tw-text-lg tw-font-bold">
-                <span>Amount (Min5.00 ETB / Max 15000.00 ETB):</span>
-
-              </label>
-              <q-input
-                v-model="state.amount"
-                filled
-                type="number"
-                lazy-rules
-                :rules="[
-                  val => val >= 5 || 'Minimum deposit amount is 5 Birr',
-                  val => val <= 15000 || 'Maximum deposit amount is 15,000 Birr',
-                ]"
-              />
-
-              <!-- <label for="available" class="tw-text-md tw-flex tw-w-full tw-flex-col tw-place-content-center tw-font-bold">
-                <span>Your telebirr account number:</span>
-
-              </label>
-              <q-input
-                v-model="state.phoneNUmber"
-                placeholder="09/07 XXX-XXX-XX"
-                type="number"
-                filled
-                lazy-rules
-              /> -->
-            </div>
-          </q-card-section>
-
-          <q-separator />
-
-          <q-card-actions align="right" class="tw-mt-4">
-            <q-btn type="submit" class="tw-w-full" :label="loading ? 'Loading...' : 'CONFIRM'" color="red-10" size="lg" />
-          </q-card-actions>
-        </q-card>
-      </q-form>
-    </div>
-  </q-dialog>
+  <div v-else>
+    <q-img v-if="imageData" fit="scale-down" :src="imageData" :alt="props.name" class="tw-h-20 tw-w-32" />
+  </div>
 </template>
