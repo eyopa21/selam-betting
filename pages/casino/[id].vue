d03e5ef307a5683bacb394b069728d53642d2ff7
job <jobtennis21@gmail.com>
2024-09-21 12:03:18 +0300

    fix: removing debugging components

--- pages/casino/[id].vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="tw-h-screen ">
-    <iframe
-      id="contentiframe"
-      src="https://client.qtlauncher.com/client/game-launcher.html#/?gameSession=c6459495-20ec-4125-a267-d8f3a44093cc&amp;hideSplash=true"
-      :allowfullscreen="true" style="width: 100%; height: 100%;"
-    />
-  </div>
-</template>
