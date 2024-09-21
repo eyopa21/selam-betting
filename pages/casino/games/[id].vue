a550347ec2743218563ac0f686ac7ed68ed7eac0
job <jobtennis21@gmail.com>
2024-09-21 12:05:37 +0300

    feat: implementing fetching games by groups done

---
+++ pages/casino/games/[id].vue
@@ -0,0 +1,151 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'casino',
+  pageType: 'authenticated',
+})
+const route = useRoute('casino-games')
+const { $authentication } = useNuxtApp()
+
+const { data: games, error } = await useFetch(`/api/casino/get-games-by-group/${route.params.id}`, {
+  method: 'GET',
+  headers: {
+    Authorization: `Bearer ${$authentication.accessToken.value}`,
+  },
+})
+if (error.value) {
+  useErrorNotifications(error)
+}
+
+const slide = ref('1')
+
+const filterType = ref<'square' | 'circle'>('square')
+const selectedGameLink = ref('')
+
+function toggleType() {
+  if (filterType.value === 'circle') {
+    filterType.value = 'square'
+  } else {
+    filterType.value = 'circle'
+  }
+}
+const tempQuery = ref('')
+const q = ref('')
+function search() {
+  q.value = tempQuery.value
+}
+const filteredGames = computed(() => {
+  if (!q.value) {
+    return games.value?.games
+  }
+
+  return games.value?.games?.filter((game) => {
+    return Object.values(game).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase())
+    })
+  })
+})
+</script>
+
+<template>
+  <div class="tw-mx-auto tw-pb-20">
+    <div class="tw-flex tw-w-full tw-gap-2 tw-p-2 ">
+      <div class="tw-w-2/3">
+        <q-carousel
+          v-model="slide" transition-prev="slide-right" transition-next="slide-left" infinite animated
+          control-color="white" navigation padding arrows height="300px" :autoplay="true"
+          class="text-white shadow-1  rounded-border"
+        >
+          <q-carousel-slide
+            name="1" img-src="/casino/casinoImage.png"
+            class="tw-h-full tw-w-full"
+          />
+        </q-carousel>
+      </div>
+      <CasinoAwards />
+    </div>
+
+    <!-- <div v-if="games?.games" class="tw-p-8">
+      <CasinoCategories :games="games?.games" @filter="filterByCategory" />
+    </div> -->
+    <div class="tw-mt-6 tw-flex tw-justify-between tw-gap-4 tw-px-8">
+      <div class="tw-self-end tw-text-lg tw-text-white" />
+      <div class="tw-flex tw-gap-2 tw-self-stretch ">
+        <input v-model="tempQuery" placeholder="Search for your Games" type="text" class="tw-w-128 tw-block tw-rounded-xl tw-border-0 tw-bg-inherit tw-py-1.5 tw-pl-7 tw-pr-20 tw-text-white  tw-ring-1 tw-ring-inset tw-ring-primary-500 placeholder:tw-text-secondary-500 focus:tw-outline-none focus:tw-ring-gray-200 sm:tw-text-sm sm:tw-leading-6">
+
+        <q-btn color="primary" rounded label="Let's Look" dense text-color="blue-grey-2" class="   tw-rounded-full tw-px-8  tw-ring-2 tw-ring-white" @click="search()" />
+      </div>
+
+      <div class="tw-flex tw-self-center tw-rounded-lg tw-border-2 tw-border-primary-400">
+        <q-btn-group outline stretch>
+          <q-btn color="primary-10" icon="filter_alt" size="lg" />
+          <q-btn color="primary-10" icon="apps" size="lg" @click="toggleType()" />
+          <q-btn color="primary-10" icon="control_camera" size="lg" />
+        </q-btn-group>
+      </div>
+    </div>
+    <div v-if="!!filteredGames?.length">
+      <div class="tw-p-8 tw-py-0">
+        <h1 class="tw-my-4 tw-text-xl tw-font-extrabold tw-capitalize tw-text-white">
+          {{ games?.name }}
+        </h1>
+        <div
+          class="tw-grid  tw-gap-4 tw-gap-y-8"
+          :class="filterType === 'square' ? 'tw-grid-cols-4' : 'tw-grid-cols-7'"
+        >
+          <div
+            v-for="(ii, k) in filteredGames" :key="k"
+            class="tw-relative tw-transition-all  tw-duration-500 hover:-tw-translate-y-2"
+          >
+            <div v-if="filterType === 'square'" class="tw-group">
+              <div class="tw-absolute tw-right-0">
+                <q-btn
+                  flat round color="white" icon="favorite_outline"
+                  class="tw-z-50 tw-transition-all tw-duration-500 group-hover:tw-scale-110"
+                />
+              </div>
+              <q-img :src="ii.logo_url" :alt="ii.label" fit="cover" class="tw-h-64  tw-rounded tw-ring tw-ring-blue-500" />
+              <div class="tw-mt-3 tw-flex tw-w-full tw-justify-between tw-space-x-4">
+                <q-btn
+                  color="deep-purple-14" label="Play" class="tw-w-full tw-rounded-xl tw-ring-2 tw-ring-white"
+                  @click="selectedGameLink = ii.play_url"
+                />
+                <q-btn
+                  color="black" label="Practice" class="tw-hidden tw-w-full tw-rounded-xl tw-ring-2 tw-ring-white lg:tw-block"
+                  @click=" selectedGameLink = ii.play_url"
+                />
+              </div>
+            </div>
+            <div v-else>
+              <q-avatar
+                font-size="52px" color="primary" text-color="white"
+                class="tw-cursor-pointer tw-ring-1 tw-transition-all tw-duration-500 hover:tw-scale-105 lg:tw-size-[170px] 2xl:tw-size-[200px]"
+              >
+                <q-img :src="ii.logo_url" />
+              </q-avatar>
+              <div class="tw-mt-3  tw-flex tw-justify-around tw-space-x-4 ">
+                <q-btn
+                  size="sm" color="deep-purple-14" label="Play" class="tw-h-6 tw-w-full tw-rounded-xl tw-ring-1 tw-ring-white"
+                  @click="selectedGameLink = ii.play_url"
+                />
+                <q-btn
+                  size="sm" color="black" label="Practice" class=" tw-hidden tw-h-6 tw-w-full tw-rounded-xl tw-ring-1 tw-ring-white lg:tw-block"
+                  @click=" selectedGameLink = ii.play_url"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="tw-flex tw-justify-center ">
+      <VUENoItemsFound :search="true" @back="tempQuery = ''; q = ''" />
+    </div>
+    <div v-if="selectedGameLink">
+      <CasinoGamePlayer :game-link="selectedGameLink" @close="selectedGameLink = ''" />
+    </div>
+
+    <div class="tw-mx-auto tw-mt-8 tw-h-full tw-w-3/4 tw-bg-primary-500">
+      <CasinoBottomAd />
+    </div>
+  </div>
+</template>
---
+++ server/api/casino/get-games-by-group/[id].ts
@@ -0,0 +1,34 @@
+import type { NuxtError } from 'nuxt/app'
+import type { GroupGamesRoot } from '~/types/casino/group-games'
+
+type ErrorResponse = {
+  detail?: string
+  error?: string
+}
+
+export default defineEventHandler(async (event) => {
+  const { id } = getRouterParams(event)
+  const config = useRuntimeConfig()
+  const url = `${config.baseApiEndpoint}/betting/casino/api/v1/get_game_by_group/${id}/`
+
+  const authHeader = getHeader(event, 'authorization')
+  try {
+    const result = await $fetch<GroupGamesRoot>(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': config.serverApiKey,
+        'Authorization': authHeader!.toString()!,
+      },
+    })
+    return result
+  } catch (err: unknown) {
+    const error = err as NuxtError
+    const errorResponse = error.data as ErrorResponse
+    throw createError({
+      statusCode: error.statusCode,
+      statusMessage: errorResponse?.detail ?? errorResponse.error ?? 'Connection Error',
+
+    })
+  }
+})
