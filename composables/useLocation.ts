c42fe5dd25b42344c29bf89047f25b22884c5320
job <jobtennis21@gmail.com>
2024-09-25 22:36:29 +0300

    feat: get location composable

---
+++ composables/useLocation.ts
@@ -0,0 +1,37 @@
+import { Notify } from 'quasar'
+
+export async function useLocation() {
+  async function getLocation(): Promise<{ lat: number, lon: number } | undefined> {
+    return new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const lat = position.coords.latitude
+            const lon = position.coords.longitude
+            resolve({ lat, lon })
+          },
+          (err) => {
+            Notify.create({
+              message: err.message,
+              icon: 'announcement',
+              color: 'red',
+            })
+            reject(err)
+          },
+        )
+      } else {
+        Notify.create({
+          message: 'Geolocation is not supported by this browser',
+          icon: 'announcement',
+          color: 'red',
+        })
+        resolve(undefined)
+      }
+    })
+  }
+
+  return {
+    getLocation,
+
+  }
+}
