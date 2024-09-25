d3a25b8a87919fd9ecdba96eee0cfcf10381862e
job <jobtennis21@gmail.com>
2024-09-25 22:38:39 +0300

    feat: request for bet for me done

---
+++ components/betForMe/account.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import type { BetForMeRoot } from '~/types/betForMe'
+
+const props = defineProps<{
+  info: BetForMeRoot
+}>()
+const { $authentication } = useNuxtApp()
+const loading = ref(false)
+async function request() {
+  loading.value = true
+  try {
+    const response = await $fetch('/api/betForMe/request', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${$authentication.accessToken.value}`,
+      },
+    })
+    if (response) {
+      useSuccessNotification('You have submitted your request, and we will notify the result soon')
+    }
+  } catch (err) {
+    useErrorNotifications(ref(err))
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div class="text-h6 tw-font-extrabold tw-leading-tight tw-tracking-tight">
+      Your Account
+    </div>
+    <div class="tw-p-8">
+      <q-avatar rounded>
+        <img src="/arsenal.jpg" alt="">
+      </q-avatar>
+      <div>
+        {{ props.info.user_account.first_name }} {{ props.info.user_account.last_name }}
+      </div>
+      Id picture
+      <div class="tw-flex tw-gap-4">
+        <q-img
+          src="/arsenal.jpg"
+          spinner-color="white"
+          style="height: 140px; max-width: 150px"
+        />
+        <q-img
+          src="/arsenal.jpg"
+          spinner-color="white"
+          style="height: 140px; max-width: 150px"
+        />
+      </div>
+      <div v-if="props.info.is_bet_for_me_agent">
+        You are aready an agent
+      </div>
+      <div class="tw-m-8">
+        <q-btn :loading="loading" @click="request()">
+          Send Request
+        </q-btn>
+      </div>
+    </div>
+  </div>
+</template>
--- server/api/betForMe/request.post.ts
+++ server/api/betForMe/request.post.ts
@@ -1,27 +1,24 @@
 import type { NuxtError } from 'nuxt/app'
 
-type LoginResult = {
-  access: string
-  refresh: string
-}
 type ErrorResponse = {
   detail: string
   Error: string
 }
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
-  const body = await readBody(event)
-  const url = `${config.restApiEndpoint}/login/`
-
+  const url = `${config.restApiEndpoint}/bet_for_me/request/`
+  const authHeader = getHeader(event, 'authorization')
   try {
-    const result = await $fetch<LoginResult>(url, {
+    const result = await $fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'X-API-KEY': config.serverApiKey,
+        'Authorization': authHeader!.toString()!,
       },
-      body,
     })
+
+    console.log('res', result)
     return result
   } catch (err: unknown) {
     const error = err as NuxtError
