f4cb7753b2350acfa6a7d5a1a87efe8d97ff98b7
job <jobtennis21@gmail.com>
2024-11-04 19:19:11 +0300

    fix: refreshtoken

---
+++ server/api/auth/refresh-token.ts
@@ -0,0 +1,34 @@
+import type { NuxtError } from 'nuxt/app'
+
+type LoginResult = {
+  access: string
+  refresh: string
+}
+type ErrorResponse = {
+  detail: string
+}
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig()
+  const body = await readBody(event)
+  const url = `${config.baseApiEndpoint}/betting/api/v1/refresh_token/`
+
+  try {
+    const result = await $fetch<LoginResult>(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': config.serverApiKey,
+      },
+      body,
+    })
+    return result
+  } catch (err: unknown) {
+    const error = err as NuxtError
+    const errorResponse = error.data as ErrorResponse
+    throw createError({
+      statusCode: error.statusCode,
+      statusMessage: errorResponse?.detail ?? 'Connection Error',
+
+    })
+  }
+})
