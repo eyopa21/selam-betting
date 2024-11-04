c6c99c5197fafdb111f564d2914605108297abca
job <jobtennis21@gmail.com>
2024-11-04 12:10:35 +0300

    feat: dynamic logo and fallback  client name

--- components/nav/logo.vue
+++ components/nav/logo.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+const general = useGeneralClientInfo()
+const logo = `${general.generalClientInfo?.nameAndLogo.logo_url}pp`
+</script>
+
 <template>
-  <div>
-    <img class="tw-w-44" src="/images/logo.png" @click="navigateTo('/')">
-  </div>
+  <ClientOnly>
+    <div v-if="logo" class=" tw-max-h-12 ">
+      <q-img class="tw-flex tw-w-44 tw-items-center tw-justify-center " :src="logo" @click="navigateTo('/')">
+        <template #loading>
+          <q-skeleton type="QBtn" />
+        </template>
+        <template #error>
+          <div class="tw-flex tw-max-h-12 tw-w-full tw-items-center tw-justify-center tw-rounded-xl tw-bg-red-500 tw-text-xs">
+            {{ general.generalClientInfo?.nameAndLogo.client_name ?? 'Cannot load logo' }}
+          </div>
+        </template>
+      </q-img>
+    </div>
+    <template #fallback>
+      <q-skeleton type="QBtn" />
+    </template>
+  </ClientOnly>
 </template>
