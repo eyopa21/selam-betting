c8eb9f7662befdcffd78acfac690b6393409785a
job <jobtennis21@gmail.com>
2024-11-06 23:53:35 +0300

    fix: better middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -24,15 +24,24 @@ export default defineNuxtRouteMiddleware((to, _) => {
     })
   }
 
-  if ((pagePackageType !== 'is_casino_game') && (!general.generalClientInfo?.general.is_live_match && !general.generalClientInfo?.general.is_pre_match)) {
-    return navigateTo('/casino')
-  }
-
-  if ((pagePackageType !== 'is_pre_match') && (!general.generalClientInfo?.general.is_casino_game && !general.generalClientInfo?.general.is_live_match)) {
+  if ((pagePackageType === 'is_casino_game') && (!general.generalClientInfo?.general.is_casino_game && general.generalClientInfo?.general.is_pre_match)) {
     return navigateTo('/')
   }
-
-  if ((pagePackageType !== 'is_live_match') && (!general.generalClientInfo?.general.is_pre_match && !general.generalClientInfo?.general.is_casino_game)) {
+  if ((pagePackageType === 'is_casino_game') && (!general.generalClientInfo?.general.is_casino_game && general.generalClientInfo?.general.is_live_match)) {
     return navigateTo('/live')
+  }
+
+  if ((pagePackageType === 'is_pre_match') && (!general.generalClientInfo?.general.is_pre_match && general.generalClientInfo?.general.is_casino_game)) {
+    return navigateTo('/casino')
+  }
+  if ((pagePackageType === 'is_pre_match') && (!general.generalClientInfo?.general.is_pre_match && general.generalClientInfo?.general.is_live_match)) {
+    return navigateTo('/live')
+  }
+
+  if ((pagePackageType === 'is_live_match') && (!general.generalClientInfo?.general.is_live_match && general.generalClientInfo?.general.is_casino_game)) {
+    return navigateTo('/casino')
+  }
+  if ((pagePackageType === 'is_live_match') && (!general.generalClientInfo?.general.is_live_match && general.generalClientInfo?.general.is_pre_match)) {
+    return navigateTo('/')
   }
 })
