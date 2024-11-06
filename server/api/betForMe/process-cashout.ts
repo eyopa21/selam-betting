25f42c2660e6aba2216ecf1a04e4e565492cb3b1
job <jobtennis21@gmail.com>
2024-11-06 09:21:23 +0300

    fix: process cashout to post method

--- server/api/betForMe/process-cashout.ts
+++ server/api/betForMe/process-cashout.ts
@@ -10,50 +10,29 @@ export type ProcessCashoutBody = {
   username: string
   amount: number
 }
-
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
-  const url = `${config.baseApiEndpoint}/betting/api/v1/cash_out_for_user/`
+  const url = `${config.baseApiEndpoint}/betting/api/v1/cash_out_for_user_request/`
   const authHeader = getHeader(event, 'authorization')
   const body: ProcessCashoutBody = await readBody(event)
   try {
-    // Convert body to JSON string
-    const bodyString = JSON.stringify(body)
-    // Construct the curl command
-    const curlCommand = `curl -X GET "${url}" \
-     -H "Content-Type: application/json" \
-     -H "Authorization: ${authHeader!.toString()!}" \
-     -H "X-API-KEY: ${config.serverApiKey}" \
-     -d '${bodyString}'`
-
-    // Execute the curl command
-    exec(curlCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing curl: ${error}`)
-        throw createError({
-          statusCode: error.code,
-          statusMessage: stderr || 'Curl Execution Error',
-        })
-      } else {
-        console.log('Cuuuurl response:', stdout)
-        // Parse response if needed
-        const result = JSON.parse(stdout)
-        if ('Error' in result) {
-          throw createError({
-            statusCode: 403,
-            statusMessage: result.Error,
-          })
-        }
-        return result
-      }
+    const result = await $fetch<string>(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': config.serverApiKey,
+        'Authorization': authHeader!.toString()!,
+      },
+      body,
     })
-  } catch (err) {
+    return result
+  } catch (err: unknown) {
     const error = err as NuxtError
     const errorResponse = error.data as ErrorResponse
-    console.log('eriiiiiiiiiiiii', error)
     throw createError({
-      statusCode: error.statusCode || 500,
+      statusCode: error.statusCode,
       statusMessage: errorResponse?.Error ?? errorResponse?.detail ?? 'Connection Error',
+
     })
   }
 })
